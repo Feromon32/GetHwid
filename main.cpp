4d97538a274bafc4a1be93885932dae7c7619412
Feromon32 <65503900+Feromon32@users.noreply.github.com>
2023-06-28 03:26:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,4 +6,7 @@ void main(void)
     HW_PROFILE_INFO   HwProfInfo;
     GetCurrentHwProfile(&HwProfInfo);
     _tprintf(HwProfInfo.szHwProfileGuid);
+    // other info
+    system("wmic path win32_physicalmedia get SerialNumber");
+    system("wmic csproduct get UUID");
 }
