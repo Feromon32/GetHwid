f64729d8224317e1b274f7baf5bcf8774fc64a37
Feromon32 <65503900+Feromon32@users.noreply.github.com>
2023-07-08 01:21:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <windows.h>
 #include <tchar.h>
 
+void setcolor(unsigned short color)
+{
+    HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hcon, color);
+}
+
 void main(void)
 {
     HW_PROFILE_INFO   HwProfInfo;
     GetCurrentHwProfile(&HwProfInfo);
+
+    system("cls");
+    setcolor(2);
     _tprintf(HwProfInfo.szHwProfileGuid);
     // other info
+    _tprintf(TEXT("\n"));
     system("wmic path win32_physicalmedia get SerialNumber");
     system("wmic csproduct get UUID");
     system("wmic diskdrive get SerialNumber");
@@ -14,24 +24,12 @@ void main(void)
     system("wmic bios get SerialNumber");
     system("wmic cpu get ProcessorId");
     system("wmic memorychip get SerialNumber");
-    system("wmic monitor get SerialNumber");
-    system("wmic netlogin get FullName");
     system("wmic os get SerialNumber");
-    system("wmic pagefile get Name");
-    system("wmic process get Name");
-    system("wmic product get Name");
-    system("wmic qfe get HotFixID");
-    system("wmic service get Name");
-    system("wmic share get Name");
-    system("wmic useraccount get Name");
     system("wmic volume get SerialNumber");
     system("wmic logicaldisk get VolumeSerialNumber");
     system("wmic nic get MACAddress");
-    system("wmic printer get Name");
-    system("wmic sounddev get Name");
-    system("wmic startup get Name");
-    system("wmic sysdriver get Name");
-    system("wmic tape get Name");
-    system("wmic timezone get Caption");
-    system("wmic userprofile get SID");
+    //system("wmic sounddev get Name"); //if need uncommnet
+    //system("wmic timezone get Caption"); //if need uncommnet
+    system("pause>nul");
+    main();
 }
